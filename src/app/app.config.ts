9944a4d03c92186a062551b547d25db5bcb089a6
gcsepregi <gabor.csepregi@4shards.org>
2025-07-29 11:01:38 +0100

    Add PWA support with service worker and web app manifest
    - Integrated `@angular/service-worker` for progressive web app functionality.
    - Added `manifest.webmanifest` with app metadata and icon definitions for PWA setup.
    - Updated `angular.json` to include service worker configuration (`ngsw-config.json`).
    - Enhanced `index.html` with noscript warning and reference to the new web manifest.
    - Updated and optimized favicon set and related assets for better platform compatibility.
    - Included service worker initialization in `app.config.ts` with production check.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import {ApplicationConfig, Provider, provideZoneChangeDetection} from '@angular/core';
+import {ApplicationConfig, Provider, provideZoneChangeDetection, isDevMode} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideToastr } from 'ngx-toastr';
@@ -6,6 +6,7 @@ import { provideToastr } from 'ngx-toastr';
 import { routes } from './app.routes';
 import {provideHttpClient, withInterceptors} from '@angular/common/http';
 import {authInterceptor} from './interceptors/auth.interceptor';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -13,6 +14,9 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideAnimations(),
     provideToastr(),
-    provideHttpClient(withInterceptors([authInterceptor])),
+    provideHttpClient(withInterceptors([authInterceptor])), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }),
   ]
 };
