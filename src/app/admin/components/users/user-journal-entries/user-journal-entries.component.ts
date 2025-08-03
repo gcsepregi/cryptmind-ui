8f142e905955e9fb2cb4e30bc416318b8d1764ac
gcsepregi <gabor.csepregi@4shards.org>
2025-08-03 14:55:26 +0100

    Add styling and dynamic table logic to UserSessionsComponent and UserJournalEntriesComponent
    - Implemented dynamic table integration for `UserSessionsComponent` and `UserJournalEntriesComponent` using `BehaviorSubject` for data handling.
    - Enhanced SCSS for components with reusable `.admin-users` styles.
    - Added back navigation links with FontAwesome icons.
    - Updated `DynamicTableComponent` to support `no-hover` row styling.

--- src/app/admin/components/users/user-journal-entries/user-journal-entries.component.ts
+++ src/app/admin/components/users/user-journal-entries/user-journal-entries.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import {AsyncPipe} from '@angular/common';
+import {DynamicTableComponent} from '../../../../common-components/components/dynamic-table/dynamic-table.component';
+import {FaIconComponent} from '@fortawesome/angular-fontawesome';
+import {RouterLink} from '@angular/router';
+import {faArrowLeft} from '@fortawesome/free-solid-svg-icons';
+import {BehaviorSubject} from 'rxjs';
 
 @Component({
   selector: 'app-user-journal-entries',
-  imports: [],
+  imports: [
+    AsyncPipe,
+    DynamicTableComponent,
+    FaIconComponent,
+    RouterLink
+  ],
   templateUrl: './user-journal-entries.component.html',
   styleUrl: './user-journal-entries.component.scss'
 })
 export class UserJournalEntriesComponent {
 
+  protected readonly columns = [];
+  protected readonly items$ = new BehaviorSubject<{[prop: string]: any}[]>([]);
+  protected readonly faArrowLeft = faArrowLeft;
 }
--- src/app/admin/components/users/user-sessions/user-sessions.component.ts
+++ src/app/admin/components/users/user-sessions/user-sessions.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import {AsyncPipe} from '@angular/common';
+import {DynamicTableComponent} from '../../../../common-components/components/dynamic-table/dynamic-table.component';
+import {UsersService} from '../../../services/users.service';
+import {BehaviorSubject} from 'rxjs';
+import {RouterLink} from '@angular/router';
+import {FaIconComponent} from '@fortawesome/angular-fontawesome';
+import {faArrowLeft} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-user-sessions',
-  imports: [],
+  imports: [
+    AsyncPipe,
+    DynamicTableComponent,
+    RouterLink,
+    FaIconComponent
+  ],
   templateUrl: './user-sessions.component.html',
   styleUrl: './user-sessions.component.scss'
 })
 export class UserSessionsComponent {
+
+  protected readonly columns = [];
+  protected readonly items$ = new BehaviorSubject<{[prop: string]: any}[]>([]);
+  protected readonly faArrowLeft = faArrowLeft;
+
+  constructor(private readonly usersService: UsersService) { }
 
 }
